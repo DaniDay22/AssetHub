5eb3d122bc179480db15583fa2f98453bb7c0e04
princzdanieloliver-dot <danigamerhd200@gmail.com>
2026-03-18 21:32:10 +0100

    Nem bírtam már tovább

--- backend/routes/StoreInventory.js
+++ backend/routes/StoreInventory.js
@@ -36,13 +36,13 @@ router.get('/All', authenticationToken, async (req, res) => {
     try{
         const {AuthLv, UserId} = req.user //Tokenből infó
 
-        if(AuthLv == 4) //Még nem biztos hogy marad: Mindeki aki nem eladó letudja kérdezni
+        if(AuthLv == 4) //Még nem biztos hogy marad: Mindenki aki nem eladó letudja kérdezni
             return res.status(403).json({ success: false, error: "Ehhez a művelethez nincs jogosultságod!" });
 
         pool = await mssql.connect(config)
 
         const result = pool.request()
-            .input('UserId', mssql.NVarChar, UserId)
+            .input('UserId', mssql.Int, UserId)
             .query(`
             SELECT
                 P.Name,
@@ -52,7 +52,6 @@ router.get('/All', authenticationToken, async (req, res) => {
                 SI.Price,
                 SI.Stock,
                 SI.Sold
-                P.Description
             FROM StoreInventory SI
             INNER JOIN Product P ON SI.ProductId = P.Id
             INNER JOIN ProductCategory PC ON P.CategoryId = PC.Id
@@ -78,7 +77,7 @@ router.get('/:PName', authenticationToken, async (req, res) => {
         let PName = req.params.PName //Beírt adat
         const {AuthLv, UserId} = req.user //Tokenből infó
 
-        if(AuthLv == 4) //Még nem biztos hogy marad: Mindeki aki nem eladó letudja kérdezni
+        if(AuthLv == 4) //Még nem biztos hogy marad: Mindenki aki nem eladó letudja kérdezni
             return res.status(403).json({ success: false, error: "Ehhez a művelethez nincs jogosultságod!" });
 
         PName = PName.trim()
@@ -88,7 +87,7 @@ router.get('/:PName', authenticationToken, async (req, res) => {
             pool = await mssql.connect(config)
 
             const result = pool.request()
-                .input('UserId', mssql.NVarChar, UserId)
+                .input('UserId', mssql.Int, UserId)
                 .input('PName', mssql.NVarChar, PName)
                 .query(`
                 SELECT
@@ -99,7 +98,6 @@ router.get('/:PName', authenticationToken, async (req, res) => {
                     SI.Price,
                     SI.Stock,
                     SI.Sold
-                    P.Description
                 FROM StoreInventory SI
                 INNER JOIN Product P ON SI.ProductId = P.Id
                 INNER JOIN ProductCategory PC ON P.CategoryId = PC.Id
@@ -112,6 +110,54 @@ router.get('/:PName', authenticationToken, async (req, res) => {
         }
 
         res.status(401).json({success: false, message: "Nem lehet speciális karakter a kérésben!"})
+    }
+    catch(err){
+        console.error("Fetch ERROR:", err)
+        res.status(500).json({success: false, message: "Szerver hiba történt!"})
+    }
+    finally{
+        if (pool) pool.close()
+    }
+})
+
+router.post('/', authenticationToken, async (req, res) => {
+    let pool;
+    try{
+        const {PName, PCName, Brand, Unit, Price, Stock, Description} = req.body
+        const {AuthLv, UserId} = req.user //Tokenből infó
+
+        if(AuthLv == 4)
+            return res.status(403).json({ success: false, error: "Ehhez a művelethez nincs jogosultságod!" });
+
+        pool = await mssql.connect(config)
+
+        const templateExists = pool.request()
+            .input('PName', mssql.NVarChar, PName.toLowerCase())
+            .input('Brand', mssql.NVarChar, Brand.toLowerCase())
+            .input('Unit', mssql.NVarChar, Unit.toLowerCase())
+            .input('PCName', mssql.NVarChar, PCName.toLowerCase())
+            .query(`
+                DECLARE @Template TABLE (
+                    Name nvarchar(100),
+                    CategoryId int,
+                    Brand nvarchar(50),
+                    Unit nvarchar(10)
+                )
+                DECLARE inCategoryId = (SELECT Id FROM ProductCategory WHERE Name = @PCName)
+
+                -- Megnézzük, hogy létezik-e ez a sablon a Products-ban
+                INSERT INTO @Template (Name, CategoryId, Brand, Unit)
+                SELECT TOP 1
+                    Name,
+                    CategoryId,
+                    Brand,
+                    Unit
+                FROM Product
+                WHERE LOWER(Name) = @PName AND LOWER(Brand) = @Brand AND LOWER(Unit) = @Unit
+                AND CategoryId = @inCategoryId
+
+                IF EXISTS (SELECT 1 )
+                `)
     }
     catch(err){
         console.error("Fetch ERROR:", err)
